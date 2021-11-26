b31284cb9a9b35fedadcdf73562261a3b8b1ba30
Andriw Martins Afonso <74825368+Andriw-afonso@users.noreply.github.com>
2021-11-26 14:55:06 -0300

    Add files via upload

--- teste-app/streamlit/app.py
+++ teste-app/streamlit/app.py
@@ -37,7 +37,7 @@ def prediction(Cholesterol,ApLo,ApHi,Smoke_yes,Age):
 def main():       
     # Config web
     html_temp = """ 
-    <div style ="background-color:yellow;padding:13px"> 
+    <div style ="background-color:gray;padding:13px"> 
     <h1 style ="color:black;text-align:center;">Diagnóstico precoce cardiovascular</h1> 
     </div> 
     """
