6a8c73fb568f8bb4fcc4c24cfe6a17c7a9029fd9
Andriw Martins Afonso <74825368+Andriw-afonso@users.noreply.github.com>
2021-11-30 09:09:54 -0300

    Add files via upload

--- teste-app/streamlit/app.py
+++ teste-app/streamlit/app.py
@@ -15,7 +15,7 @@ def prediction(Cholesterol,ApLo,ApHi,Smoke_yes,Age):
   df=json.dumps(df.to_dict(orient='records'))
 
   #API call
-  path='https://sdkfsdnfzzzzz.herokuapp.com/'
+  path='https://hghghgdddddd.herokuapp.com/'
   url=path +'oi'
   headers={'content-type':'application/json'}
   data=df
