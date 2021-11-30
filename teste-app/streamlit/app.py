2adcb2fc35db4920a32d498566da93e236a89bfd
Andriw Martins Afonso <74825368+Andriw-afonso@users.noreply.github.com>
2021-11-30 12:47:21 -0300

    Update app.py

--- teste-app/streamlit/app.py
+++ teste-app/streamlit/app.py
@@ -57,7 +57,7 @@ def main():
     # Predict 
     if st.button("Predict"):        
         result = prediction(Cholesterol,ApLo,ApHi,Age) 
-        st.success('Estado de saúde: {}'.format(Cholesterol,ApLo,ApHi,Age))
+        st.success('Estado de saúde: {}'.format([Cholesterol,ApLo,ApHi,Age]))
         
      
 if __name__=='__main__': 
